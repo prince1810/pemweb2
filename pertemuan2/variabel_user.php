6bbe24be79a7c6a4e747152d93e669f47a149e54
muhammad fathi farhat <mufahat1810@gmail.com>
2025-03-07 11:00:46 +0700

    update pertemuan 2

--- pertemuan2/variabel_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // medefinisikan variabel
-    $nama = "fathi";
-    $usia = 21;
-    $status_menikah = false;
-    $alamat = "Jl. Raya Jakarta";
-    $hobi = array("makan", "minum", "bermain");
-?>
-```
